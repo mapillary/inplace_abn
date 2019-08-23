6b2b114aff91c4d7703764c1db46b09c967593d4
Lorenzo Porzi <lorenzo@mapillary.com>
2019-08-23 10:25:39 +0200

    Fix compilation for systems that have CUDA sdk but no CUDA devices

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-if torch.cuda.is_available():
+if torch.has_cuda:
     ext_modules = [
         CUDAExtension(
             name="inplace_abn._backend",
