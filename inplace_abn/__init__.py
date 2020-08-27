f2dde7e45576089107866a55194d4971faab3502
Lorenzo Porzi <lorenzo@mapillary.com>
2020-08-27 16:39:58 +0200

    Fix import errors when using without running setup.py first

--- inplace_abn/__init__.py
+++ inplace_abn/__init__.py
@@ -1,3 +1,7 @@
-from ._version import version as __version__
 from .abn import ABN, InPlaceABN, InPlaceABNSync
 from .group import active_group, set_active_group
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    pass
